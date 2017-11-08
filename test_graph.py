bba2ad403822fd795f82045da4e7c4c12f6ec5ec
StoneInc <levyehrstein@googlemail.com>
2017-11-08 01:17:53 +0100

    Further work on visualization

--- test_graph.py
+++ test_graph.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from pyqtgraph.Qt import QtGui, QtCore
+from numpy import asarray
+from PIL import Image
 import time
+import math
 import random
 import threading
 import numpy as np
@@ -14,49 +17,62 @@ application = QtGui.QApplication([])
 window = pg.GraphicsWindow()
 window.resize(1000,600)
 window.setWindowTitle('BAMBICAR RECORDER')
+layout = pg.GraphicsLayout()
+window.addItem(layout)
 
-
-plot_thrust = window.addPlot(0, 0, title="Accelerator")
+plot_thrust = layout.addPlot(title="Accelerator", row=0, col=0, colspan=2)
 plot_thrust.setYRange(-100, 100, padding=0)
+plot_thrust.addItem(pg.InfiniteLine(0, angle=0, pen="w"))
 plot_thrust_curve = plot_thrust.plot(pen="y")
 
-
-plot_steering = window.addPlot(1, 0, title="Steering wheel angle")
+plot_steering = layout.addPlot(title="Steering wheel angle", row=1, col=0)
 plot_steering.setYRange(-360, 360, padding=0)
+plot_steering.showAxis('right')
+plot_steering.addItem(pg.InfiniteLine(0, angle=0, pen="w"))
 plot_steering_curve = plot_steering.plot(pen="y")
 
+image_wheel = Image.open('wheel.png')
+plot_wheel = pg.ImageItem(asarray(image_wheel))
+plot_wheelPlt = layout.addPlot(1, 1)
+plot_wheelPlt.hideAxis('left')
+plot_wheelPlt.hideAxis('bottom')
+plot_wheelPlt.addItem(plot_wheel)
+
+
+data_thrust_x = []
+data_thrust_y = []
+data_steering_x = []
+data_steering_y = []
+
+time_start = time.time()
+def addData(thrust, steering):
+	time_now = time.time()
+	time_diff = time_now - time_start
+
+	data_thrust_x.append(time_diff)
+	data_steering_x.append(time_diff)
+	data_thrust_y.append(thrust)
+	data_steering_y.append(steering)
+
+	plot_thrust_curve.setData(data_thrust_x[-500:], data_thrust_y[-500:])
+	plot_steering_curve.setData(data_steering_x[-500:], data_steering_y[-500:])
+
+	plot_wheel.setImage(asarray(image_wheel.rotate(-data_steering_y[-1] - 90)))
+
+	if (len(data_thrust_x) % 10 == 0):
+		window.setWindowTitle('BAMBICAR RECORDER [RECORDED ' + str(len(data_thrust_x)) + ' DATASETS]')
+
+	application.processEvents()
+	
 try:
-####################################################
-####################################################
-####################################################
-
+	addData(0,0)
 	# Main loop
-	data_thrust = [0]
-	data_steering = [0]
-	start = time.time()
-	for i in range(0, 100):
-		data_thrust.append(max(min(data_thrust[-1] + random.uniform(-10, 10), 100), -100))
-		data_steering.append(max(min(data_steering[-1] + random.uniform(-36, 36), 360), -360))
+	for i in range(0, 100000):
+		addData(max(min(data_thrust_y[-1] + random.uniform(-5, 5), 100), -100),
+				max(min(data_steering_y[-1] + random.uniform(-5, 5), 355), -355))
+		time.sleep(0.05)
 
-		plot_thrust_curve.setData(y=data_thrust)
-		plot_steering_curve.setData(y=data_steering)
 
-		application.processEvents()
-
-		sinceLastFrame = time.time() - start - i * 0.1
-		timeToSleep = 0.1 - sinceLastFrame
-
-		if (timeToSleep < 0):
-			print("Cant ceep up!")
-		else:
-			time.sleep(timeToSleep)
-
-	end = time.time()
-	print("Done [10SEC; " + str(end-start) +"]")
-		
-####################################################
-####################################################
-####################################################
 except KeyboardInterrupt:
 	print("Aborted.")
 
