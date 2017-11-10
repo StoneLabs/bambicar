1c2144c5ae261a16ef9f38e8edc6a8c817935395
StoneInc <levyehrstein@googlemail.com>
2017-11-11 00:30:21 +0100

    Added speed graph

--- test_graph.py
+++ test_graph.py
@@ -20,13 +20,17 @@ window.setWindowTitle('BAMBICAR RECORDER')
 layout = pg.GraphicsLayout()
 window.addItem(layout)
 
-plot_thrust = layout.addPlot(title="Accelerator", row=0, col=0, colspan=2)
+plot_thrust = layout.addPlot(title="Accelerator", row=0, col=0, colspan=1)
 plot_thrust.setYRange(-100, 100, padding=0)
 plot_thrust.addItem(pg.InfiniteLine(0, angle=0, pen="w"))
 plot_thrust_curve = plot_thrust.plot(pen="y")
 
+plot_speed = layout.addPlot(title="Speed", row=0, col=1, colspan=1)
+plot_speed.setYRange(0, 220, padding=0)
+plot_speed_curve = plot_speed.plot(pen="y")
+
 plot_steering = layout.addPlot(title="Steering wheel angle", row=1, col=0)
-plot_steering.setYRange(-360, 360, padding=0)
+plot_steering.setYRange(-540, 540, padding=0)
 plot_steering.showAxis('right')
 plot_steering.addItem(pg.InfiniteLine(0, angle=0, pen="w"))
 plot_steering_curve = plot_steering.plot(pen="y")
@@ -39,21 +43,26 @@ plot_wheelPlt.hideAxis('bottom')
 plot_wheelPlt.addItem(plot_wheel)
 
 
+data_speed_x = []
+data_speed_y = []
 data_thrust_x = []
 data_thrust_y = []
 data_steering_x = []
 data_steering_y = []
 
 time_start = time.time()
-def addData(thrust, steering):
+def addData(thrust, steering, speed):
 	time_now = time.time()
 	time_diff = time_now - time_start
 
+	data_speed_x.append(time_diff)
 	data_thrust_x.append(time_diff)
 	data_steering_x.append(time_diff)
+	data_speed_y.append(speed)
 	data_thrust_y.append(thrust)
 	data_steering_y.append(steering)
 
+	plot_speed_curve.setData(data_speed_x[-500:], data_speed_y[-500:])
 	plot_thrust_curve.setData(data_thrust_x[-500:], data_thrust_y[-500:])
 	plot_steering_curve.setData(data_steering_x[-500:], data_steering_y[-500:])
 
@@ -65,11 +74,12 @@ def addData(thrust, steering):
 	application.processEvents()
 	
 try:
-	addData(0,0)
+	addData(0,0,0)
 	# Main loop
 	for i in range(0, 100000):
 		addData(max(min(data_thrust_y[-1] + random.uniform(-5, 5), 100), -100),
-				max(min(data_steering_y[-1] + random.uniform(-5, 5), 355), -355))
+				max(min(data_steering_y[-1] + random.uniform(-5, 5), 540), -540),
+				max(min(data_speed_y[-1] + random.uniform(-5, 5), 200), 000))
 		time.sleep(0.05)
 
 
--- test_read.py
+++ test_read.py
@@ -9,4 +9,4 @@ cmd = obd.commands.SPEED # select an OBD command (sensor)
 response = connection.query(cmd) # send the command, and parse the response
 
 print(response.value) # returns unit-bearing values thanks to Pint
-print(response.value.to("mph")) # user-friendly unit conversions
+print(response.value.to("kph")) # user-friendly unit conversions
